08beec061a69066a49e9cd29c726bea54a92282e
Guido Antonio Cano Gamarra <guido.cano@kyndryl.com>
2023-10-13 21:56:50 -0500

    Se agrega archivo aws primera conexion

---
+++ css/Myfirstconnectionaws.py
@@ -0,0 +1,36 @@
+import configparser
+
+# Crea un objeto de configuración
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Obtén las credenciales 
+aws_access_key_id = config['default']['aws_access_key_id']
+aws_access_key_id = config['default']['aws_secret_access_key']
+region_name = config['default']['region_name']
+
+
+# Crea un objeto de sesión de AWS con las credenciales obtenidas del archivo
+
+session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name=region_name
+)
+
+# Crea un objeto de cliente de EC2
+
+ec2 = session.client('ec2')
+
+# Obtén la lista de instancias EC2
+
+instances = ec2.describe_instances()
+
+# Imprime la lista de instancias EC2
+
+print(instances)
+
+
+
+
